630652f1d3983b6cda8e15ec67e9f32f5514991f
moises <moyses_theboy@hotmail.com>
2018-11-09 15:54:51 -0600

    prueba de archivos

---
+++ subirmain18161135.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class subirmain18161135 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class subirmain18161135
+{
+    public static void main (String [] args ){
+    int p=3;
+    System.out.println("dbfuisbg: "+p);
+    
+    }
+}
